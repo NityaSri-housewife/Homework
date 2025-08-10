20beb0430dc71f1d90f949a33819f20478cba330
NityaSri-housewife <nityasrivavilala@gmail.com>
2025-08-10 14:42:45 +0530

    Update Secret.py

--- Secret.py
+++ Secret.py
@@ -12,3 +12,20 @@ PASSWORD = "your_email_password"
 RECEIVER = "receiver_email@example.com"
 
 AUTO_TRADE = false
+
+
+[DHAN]
+CLIENT_ID = "1107984806"
+ACCESS_TOKEN = "your_dhan_access_token"
+
+[TELEGRAM]
+BOT_TOKEN = "8133685842:AAGdHCpi9QRIsS-fWW5Y1ArgKJvS95QL9xU"
+CHAT_ID = "5704496584"
+
+[EMAIL]
+USER = "nityasrivavilala@gmail.com"
+PASSWORD = "vivj zubk gjgj kpgz"
+RECEIVER = "nityasrivavalala.gmail.com"
+
+AUTO_TRADE = True
+ 
