5852248233ad16f5ff2db25c2a7ae077d21caa70
NityaSri-housewife <nityasrivavilala@gmail.com>
2025-08-29 12:40:04 +0530

    Update oldisgold.py

--- oldisgold.py
+++ oldisgold.py
@@ -61,9 +61,10 @@ def determine_pcr_level(pcr_value):
     elif pcr_value >= 1.5: return "Support", "Strike price +20"
     elif pcr_value >= 1.2: return "Support", "Strike price -20"
     elif 0.71 <= pcr_value <= 1.19: return "Neutral", "0"
-    elif pcr_value <= 0.7 and pcr_value > 0.59: return "Resistance", "Strike price +20"    
-    elif pcr_value <= 0.5 and pcr_value > 0.4: return "Resistance", "Strike price +15"
-    elif pcr_value <= 0.4 and pcr_value > 0.3: return "Resistance", "Strike price -10"
+    elif pcr_value <= 0.7 and pcr_value > 0.78: return "Resistance", "Strike price +20"    
+    elif pcr_value <= 0.78 and pcr_value > 0.59: return "Resistance", "Strike price -20"   
+    elif pcr_value <= 0.59 and pcr_value > 0.4: return "Resistance", "Strike price -25"
+    elif pcr_value <= 0.4 and pcr_value > 0.3: return "Resistance", "Strike price -20"
     elif pcr_value <= 0.3 and pcr_value > 0.2: return "Strong Resistance", "Strike price -20"
     else: return "Strong Resistance", "Strike price +25"
 
