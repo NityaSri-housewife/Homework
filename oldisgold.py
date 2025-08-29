2b89b1d9feedabcd7850f0f11aacee4745523260
NityaSri-housewife <nityasrivavilala@gmail.com>
2025-08-29 15:03:40 +0530

    Update oldisgold.py

--- oldisgold.py
+++ oldisgold.py
@@ -57,14 +57,14 @@ def calculate_pcr(pe_oi, ce_oi):
     return pe_oi / ce_oi if ce_oi != 0 else float('inf')
 
 def determine_pcr_level(pcr_value):
-    if pcr_value >= 3: return "Strong Support", "Strike price +30"
-    elif pcr_value >= 2: return "Strong Support", "Strike price +30"
-    elif pcr_value >= 1.5: return "Support", "Strike price +25"
+    if pcr_value >= 3: return "Strong Support", "Strike price +25"
+    elif pcr_value >= 2: return "Strong Support", "Strike price +20"
+    elif pcr_value >= 1.5: return "Support", "Strike price +5"
     elif pcr_value >= 1.2: return "Support", "Strike price -20"
     elif 0.71 <= pcr_value <= 1.19: return "Neutral", "0"
     elif pcr_value <= 0.7 and pcr_value > 0.78: return "Resistance", "Strike price +20"    
-    elif pcr_value <= 0.78 and pcr_value > 0.59: return "Resistance", "Strike price -20"   
-    elif pcr_value <= 0.59 and pcr_value > 0.4: return "Resistance", "Strike price -30"
+    elif pcr_value <= 0.78 and pcr_value > 0.59: return "Resistance", "Strike price +5"   
+    elif pcr_value <= 0.59 and pcr_value > 0.4: return "Resistance", "Strike price -20"
     elif pcr_value <= 0.4 and pcr_value > 0.3: return "Resistance", "Strike price -20"
     elif pcr_value <= 0.3 and pcr_value > 0.2: return "Strong Resistance", "Strike price -20"
     else: return "Strong Resistance", "Strike price +25"
